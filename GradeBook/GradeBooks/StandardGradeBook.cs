becada1e9aa825af5953766e13f8cf90adcb1621
Rentag <h.gery94@gmail.com>
2019-11-22 14:14:17 +0100

    module 1 completed

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
